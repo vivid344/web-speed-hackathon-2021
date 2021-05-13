13458fa05b5e43908443171a6cdbd25620065dcc
vivid344 <vivid344@icloud.com>
2021-05-13 19:55:28 +0900

    Fix: Add cssnano

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,13 +1,16 @@
 const postcssImport = require('postcss-import');
 const postcssPresetEnv = require('postcss-preset-env');
 const tailwindcss = require('tailwindcss');
+const cssnano = require('cssnano');
 
 module.exports = {
+  map: false,
   plugins: [
     postcssImport(),
     tailwindcss(),
     postcssPresetEnv({
       stage: 3,
     }),
+    cssnano(),
   ],
 };
