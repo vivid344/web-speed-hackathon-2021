b1ff7977f9891a2a6a6be1e0451b801f4da59721
vivid344 <vivid344@icloud.com>
2021-05-13 21:49:18 +0900

    Fix: scroll check count

--- client/src/components/foundation/InfiniteScroll/InfiniteScroll.jsx
+++ client/src/components/foundation/InfiniteScroll/InfiniteScroll.jsx
@@ -13,7 +13,7 @@ const InfiniteScroll = ({ children, fetchMore }) => {
   React.useEffect(() => {
     const handler = () => {
       // 念の為 2の18乗 回、最下部かどうかを確認する
-      const hasReached = Array.from(Array(2 ** 18), () => {
+      const hasReached = Array.from(Array(1), () => {
         return window.innerHeight + Math.ceil(window.scrollY) >= document.body.offsetHeight;
       }).every(Boolean);
 
