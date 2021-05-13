a6a36c682a5a41844a852a8cd4132dc1077f4f63
vivid344 <vivid344@icloud.com>
2021-05-14 00:22:49 +0900

    Update: use shrink-rai

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import bodyParser from 'body-parser';
 import Express from 'express';
 import session from 'express-session';
+import shrinkRay from 'shrink-ray-current';
 
 import { apiRouter } from './routes/api';
 import { staticRouter } from './routes/static';
@@ -19,6 +20,7 @@ app.use(
 );
 app.use(bodyParser.json());
 app.use(bodyParser.raw({ limit: '10mb' }));
+app.use(shrinkRay());
 
 app.use((_req, res, next) => {
   res.header({
